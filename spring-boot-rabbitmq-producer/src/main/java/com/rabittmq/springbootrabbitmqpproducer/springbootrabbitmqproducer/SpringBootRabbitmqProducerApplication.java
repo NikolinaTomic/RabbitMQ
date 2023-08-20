bc47c648fcea8dd762d587994abd26bb8b283c0c
NikolinaTomic <44821513+NikolinaTomic@users.noreply.github.com>
2023-08-20 20:06:02 +0200

    Update SpringBootRabbitmqProducerApplication.java

--- spring-boot-rabbitmq-producer/src/main/java/com/rabittmq/springbootrabbitmqpproducer/springbootrabbitmqproducer/SpringBootRabbitmqProducerApplication.java
+++ spring-boot-rabbitmq-producer/src/main/java/com/rabittmq/springbootrabbitmqpproducer/springbootrabbitmqproducer/SpringBootRabbitmqProducerApplication.java
@@ -3,9 +3,6 @@ package com.rabittmq.springbootrabbitmqpproducer.springbootrabbitmqproducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
-
 @SpringBootApplication
 public class SpringBootRabbitmqProducerApplication {
 
