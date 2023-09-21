b891c5309a96357a61b3b511aa4b7cf74a92ca55
NikolinaTomic <44821513+NikolinaTomic@users.noreply.github.com>
2023-09-22 00:24:36 +0200

    Update SpringBootRabbitmqConsumerApplication.java

--- spring-boot-rabbitmq-consumer/src/main/java/com/rabittmq/springbootrabbitmqproducer/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplication.java
+++ spring-boot-rabbitmq-consumer/src/main/java/com/rabittmq/springbootrabbitmqproducer/springbootrabbitmqconsumer/SpringBootRabbitmqConsumerApplication.java
@@ -10,17 +10,6 @@ import java.util.concurrent.TimeoutException;
 public class SpringBootRabbitmqConsumerApplication {
 
     public static void main(String[] args) throws IOException, TimeoutException {
-//		ConnectionFactory factory = new ConnectionFactory();
-//		Connection connection = factory.newConnection();
-//		Channel channel = connection.createChannel();
-//		var queueName = "hello world";
-//		channel.queueDeclare(queueName, false, false, false, null);
-//
-//		channel.basicConsume(queueName, true, (s, delivery) -> {
-//			String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-//			System.out.println("Received message: " + message);
-//		}, consumerTag -> {
-//		});
         SpringApplication.run(SpringBootRabbitmqConsumerApplication.class, args);
     }
 
